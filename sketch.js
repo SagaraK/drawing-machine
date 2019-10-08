f264c5b62fd81c738ae6eec25118c03cb9158b6d
SagaraK <43074699+SagaraK@users.noreply.github.com>
2019-10-07 21:19:04 -0700

    noise

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function  setup() {
-createCanvas(600,600);
-background(0);
-strokeWeight(5);
+createCanvas(windowWidth, windowHeight);
+background(200,50,133);
 }
 
 function draw() {
-  if(mouseIsPressed){
-    stroke(map(mouseX,0,600,0,255,true))
+    background(220,50,133,5);
+    strokeWeight(strokeWidth);
+    noiseOffset =+ 0.05;
+    strokeWidth = noise(noiseOffset) * 100;
+    stroke(map(mouseX,0,600,0,255,true));
     line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
     line(mouseX, mouseY, pmouseX, pmouseY);
-  }
 }
+
 
 function keyTyped(){
   if(key==='s'){
