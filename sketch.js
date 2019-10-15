8be089dcc1f1c9630fd5bba2d12b45405cdd4ce1
SagaraK <43074699+SagaraK@users.noreply.github.com>
2019-10-15 12:41:25 -0700

    style chANGE

--- sketch.js
+++ sketch.js
@@ -1,25 +1,69 @@
-let noiseOffset = 0.0;
-let strokeWidth = 5;
-
+let setting = 0;
 function  setup() {
-createCanvas(windowWidth, windowHeight);
+createCanvas(400, 400);
 background(200,50,133);
+drawGrid();
+keyTyped();
 }
 
 function draw() {
-    background(220,50,133,5);
-    strokeWeight(strokeWidth);
-    noiseOffset =+ 0.05;
-    strokeWidth = noise(noiseOffset) * 100;
-    stroke(map(mouseX,0,600,0,255,true));
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    console.log(mouseX);
+    let r = random(50);
+    if (mouseIsPressed && setting == 0){
+      background(random(255),random(255),random(255),1);
+      strokeWeight(5);
+      stroke(map(mouseX,0,0,0,0,true));
+      line(random(width) + mouseX, random(height) - mouseY, random(width) - pmouseX, random(height) + pmouseY);
+    }
+    else if(mouseIsPressed && setting == 1){
+      background(random(255),random(255),random(255),1);
+      strokeWeight(5);
+      //stroke(map(mouseX,0,0,0,0,true));
+      line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+      //line(mouseX - r , mouseY + r, pmouseX - r,  pmouseY + r);
+    }
+    else if(mouseIsPressed && setting == 2){
+        background(random(255),random(255),random(255),1);
+        strokeWeight(r);
+        line(mouseX - r , mouseY + r, pmouseX - r,  pmouseY + r);
+        //line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+
+
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 }
 
+function drawGrid(){
+  strokeWeight(1);
+  numCells = 20;
 
+  for (let i = 0; i<= width; i+= width/numCells){
+    for (let j = 0; j <= height; j += height / numCells){
+      rect(i,j,width/numCells, height/ numCells);
+    }
+  }
+}
+
+/*function styleChange(){
+  if(key === 'a'){
+    setting += 1;
+  }
+  else{
+    return false;
+  }
+
+}*/
 function keyTyped(){
   if(key==='s'){
     saveCanvas('fileName','png');
+  }
+  else if(key === 'a'){
+    if(setting == 2){
+      setting = 0;
+    }
+    else{
+      setting += 1;
+    }
   }
   return false;
 }
